e8dab512e7346ea06a2db6c3f6b3644f2f75eb9a
bouygun <berceozumm@gmail.com>
2024-07-23 13:53:05 +0300

    feat: remove the webhook url

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
 import java.io.IOException;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws IOException {
         Discord discord = new Discord();
 // env almalısın:
-        discord.webhookUrl = "https://discord.com/api/webhooks/1265183261328674817/CMMZLWX0u74nPhyHsMYsM6WVrAJZICwp0vf7_NJMCvlWyuF0xvadoVgTaAynfuIUAYl3";
-        discord.userName = "Özüm 123 Deneme";
+        discord.webhookUrl = "https://discord.com/api/webhooks/webhooklinkiniz";
+        discord.userName = "123 Deneme";
         discord.content = "Java ile Discord Webhook Kullanarak Deneme Mesajı Atıyorum!!";
 
 // algoritmayı böldük. mesaj içeriği - mesajı gönderme iki ayrı class böldük.
